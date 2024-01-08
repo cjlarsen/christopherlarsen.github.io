75fb231a598d0375aa14f9173cc8935f8281708a
Chris Larsen <chris@theblacknumbers.com>
2024-01-07 18:59:26 -0600

    Move personal site over

---
+++ js/openFancyBox.js
@@ -0,0 +1,223 @@
+$(".open_fancybox_bioworld").click(function() {
+    
+    $.fancybox.open([
+        {
+            href : 'img/galleries/bioworld/fm1.jpg'
+        },
+                {
+            href : 'img/galleries/bioworld/fm2.jpg'
+        },
+                {
+            href : 'img/galleries/bioworld/fm3.jpg'
+        },
+                {
+            href : 'img/galleries/bioworld/fm4.jpg'
+        }
+    ], {
+        padding : 0
+    });
+    
+    return false;
+    
+});
+
+$(".open_fancybox_cratebind").click(function() {
+    
+    $.fancybox.open([
+        {
+            href : 'img/galleries/cratebind/stricct.jpg'
+        }
+    ], {
+        padding : 0
+    });
+    
+    return false;
+    
+});
+
+$(".open_fancybox_vimeo").click(function() {
+    
+    $.fancybox.open([
+        {
+            href : 'img/galleries/vimeo/vim_iOS.jpg'
+        },
+        {
+            href : 'img/galleries/vimeo/player.jpg'
+        },
+        {
+            href : 'img/galleries/vimeo/hubnut.jpg'
+        },
+        {
+            href : 'img/galleries/vimeo/outro.jpg',                
+        }
+    ], {
+        padding : 0
+    });
+    
+    return false;
+    
+});
+
+$(".open_fancybox_etr").click(function() {
+    
+    $.fancybox.open([
+        {
+            href : 'img/galleries/etr/DOND_1.jpg',
+            title : 'Deal or No Deal Slots'
+        },
+        {
+            href : 'img/galleries/etr/DOND_2.jpg',
+            title : 'Deal or No Deal Slots'
+        },
+        {
+            href : 'img/galleries/etr/DOND_3.jpg',
+            title : 'Deal or No Deal Slots'              
+        },
+        {
+            href : 'img/galleries/etr/espnRadio.jpeg',
+            title : 'ESPN Radio iOS App (iPhone and iPad)'                
+        },
+        {
+            href : 'img/galleries/etr/read180.png',
+            title : 'Scholastic Read 180'            
+        },
+        {
+            href : 'img/galleries/etr/espnMoto.jpg',
+            title : 'TG Motocross'
+
+        }
+    ], {
+        padding : 0
+    });
+    
+    return false;
+    
+});
+
+$(".open_fancybox_lbi").click(function() {
+    
+    $.fancybox.open([
+        {
+            href : 'img/galleries/lbi/BAApp.jpg',
+            title : 'Beauty Advisor AS3 Air App for Android Tablets'
+        },
+        {
+            href : 'img/galleries/lbi/Coke_Olympics.jpg',
+            title: 'Coca Cola / Olympics'
+        },
+        {
+            href : 'img/galleries/lbi/milagro.jpg',
+            title : 'Milagro / William Grant and Sons'              
+        }
+    ], {
+        padding : 0
+    });
+    
+    return false;
+    
+});
+
+$(".open_fancybox_other").click(function() {
+    
+    $.fancybox.open([
+        {
+            href : 'img/galleries/other/steelpanther.jpg',
+            title : 'Steel Panther (band) design and flash dev'
+        },
+        {
+            href : 'img/galleries/other/verve.jpg',
+            title : 'Verve christmas album design and flash dev'
+        },
+        {
+            href : 'img/galleries/other/wackness.jpg',
+            title : 'The Wackness movie website, design'
+        },
+        {
+            href : 'img/galleries/other/entranceband.jpg',
+            title : 'Entrance Band design and dev'              
+        },
+        {
+            href : 'img/galleries/other/capella.jpg',
+            title : 'Capella luxury condos, design and flash dev'
+        },
+        {
+            href : 'img/galleries/other/waltzwithbashir.jpg',
+            title : 'Waltz With Bashir movie website, design'
+        },
+        {
+            href : 'img/galleries/other/obbp.jpg',
+            title : 'One Brooklyn Bridge Park website design and flash dev'
+        }
+    ], {
+        padding : 0
+    });
+    
+    return false;
+    
+});
+
+$(".open_fancybox_freelance").click(function() {
+    
+    $.fancybox.open([
+        {
+            href : 'img/galleries/freelance/entropy1.jpg',
+            title : 'Illustration and design for album layout'           
+        },
+        {
+            href : 'img/galleries/freelance/gnlabel.jpg',
+            title : 'Graphic Nature Label website design and front end development'
+        },
+        {
+            href : 'img/galleries/freelance/banquets_1.jpg',
+            title : '7inch album cover'
+        },
+        {
+            href : 'img/galleries/freelance/banquets_2.jpg',
+            title : 'Full length album layout'
+        },
+        {
+            href : 'img/galleries/freelance/banquets_3.jpg',
+            title : 'Illustration and flier layout for a show'           
+        },
+        {
+            href : 'img/galleries/freelance/ac.jpg',
+            title : 'Portfolio website design and build (HTML/CSS/JQuery'
+        }
+    ], {
+        padding : 0
+    });
+    
+    return false;
+    
+});
+
+$(".open_fancybox_personal").click(function() {
+    
+    $.fancybox.open([
+        {
+            href : 'img/galleries/personal/sc.jpg',
+            title : 'Strength Coach iOS App'
+        },
+        {
+            href : 'img/galleries/personal/bigbill_biglou.jpg',
+            title : 'Oil and mixed media paintings'
+        },
+        {
+            href : 'img/galleries/personal/multiblt.jpg',
+            title : 'iOS App for scanning and testing ble and bluetooth classic devices'
+        },
+        {
+            href : 'img/galleries/personal/john_cash.jpg',
+            title : 'Mixed media painting of Johnny Cash'
+        },
+        {
+            href : 'img/galleries/personal/tourmanager_app.jpg',
+            title : 'Tour Manager app, coming someday'
+        },
+    ], {
+        padding : 0
+    });
+    
+    return false;
+    
+});
